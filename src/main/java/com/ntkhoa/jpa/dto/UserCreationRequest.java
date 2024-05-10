27c88bf8b61ecbece113984ced3896b7b3386266
Tankhoa15 <tankhoa1503kh@gmail.com>
2024-05-10 21:57:34 +0700

    validation for username

--- src/main/java/com/ntkhoa/jpa/dto/UserCreationRequest.java
+++ src/main/java/com/ntkhoa/jpa/dto/UserCreationRequest.java
@@ -10,10 +10,12 @@ import java.time.LocalDate;
 @Getter
 public class UserCreationRequest {
 
+    @Size(min = 3, message = "Username must be at least 3 character!")
     private String username;
 
     @Size(min = 8, message = "Password must be at least 8 character!")
     private String password;
+
     private String firstName;
     private String lastName;
     private LocalDate dob;
