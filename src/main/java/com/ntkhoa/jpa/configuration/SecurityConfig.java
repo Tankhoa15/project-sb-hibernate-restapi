1068a1d77b1d503a838e3c7b571509e4a77cea1c
Tankhoa15 <tankhoa1503kh@gmail.com>
2025-02-12 17:33:35 +0700

    "config authen logout"

--- src/main/java/com/ntkhoa/jpa/configuration/SecurityConfig.java
+++ src/main/java/com/ntkhoa/jpa/configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.ntkhoa.jpa.configuration;
 
 import com.ntkhoa.jpa.enums.Role;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,20 +33,19 @@ public class SecurityConfig {
         "/auth/logout"
     };
 
-    @Value("${jwt.signerKey}")
-    private String signerKey;
+    @Autowired
+    private CustomJwtDecoder customJwtDecoder;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(request ->
                 request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).permitAll()
-//                        .requestMatchers(HttpMethod.GET,"/users")
-//                            .hasRole(Role.ADMIN.name())
                     .anyRequest().authenticated()
         );
 
         httpSecurity.oauth2ResourceServer(oauth2 ->
-                        oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder())
+                        oauth2.jwt(jwtConfigurer ->
+                                jwtConfigurer.decoder(customJwtDecoder)
                             .jwtAuthenticationConverter(jwtAuthenticationConverter()))
         );
 
@@ -62,15 +62,6 @@ public class SecurityConfig {
         return jwtAuthenticationConverter;
     }
 
-
-    @Bean
-    JwtDecoder jwtDecoder(){
-        SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(),"HS512");
-        return NimbusJwtDecoder
-                .withSecretKey(secretKeySpec)
-                .macAlgorithm(MacAlgorithm.HS512)
-                .build();
-    }
 
     @Bean
     PasswordEncoder passwordEncoder(){
